3be78399dbd62ca931950b972134e9f997ba649d
115design <115.design.studio@gmail.com>
2016-09-06 01:55:57 +0900

    設定ファイル変更 scss更新

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -5,12 +5,12 @@ var pngquant = require('imagemin-pngquant');
  
 // 画像圧縮処理
 gulp.task('imagemin', function() {
-    gulp.src('./dist/img/**/*.+(jpg|jpeg|png|gif|svg)')
+    gulp.src('./dist/images/**/*.+(jpg|jpeg|png|gif|svg)')
       .pipe(imagemin({
           progressive: true,
           use: [pngquant({quality: '65-80', speed: 1})]
       }))
-      .pipe(gulp.dest('./img/'));
+      .pipe(gulp.dest('./dist/images/'));
     // gulp.src('img/**/*.{png,jpg,jpeg}')
     //   .pipe(tinypng({
     //     key: '',
